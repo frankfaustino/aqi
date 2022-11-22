a9b73680cea9a3b9448d5f53ade6065e3a752ef9
Frank Faustino <frank.faustino@gmail.com>
2022-11-21 23:42:23 -0800

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 just playing around with rust
+
+## requirements
+
+- [Rust](https://www.rust-lang.org/)
+- [AQI Open Data Platform token](https://aqicn.org/data-platform/token/)
+
+## run
+
+```bash
+git clone https://github.com/frankfaustino/aqi.git
+
+cd aqi
+
+cargo build
+
+# get AQI for specified city
+cargo run -- --token TOKEN info "Las Vegas"
+
+# get cities for specified region
+cargo run -- --token TOKEN search "Philippines"
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use color_eyre::Result;
+// use inflector::Inflector;
 use serde::Deserialize;
 use structopt::StructOpt;
 
-// a struct that is converted from command line arguments
-// and environment variablse
+// a struct that is converted from command line args and env vars
 #[derive(StructOpt)]
 struct Aqi {
     // sets cli flags and env var as fallback
@@ -21,8 +21,28 @@ struct Aqi {
 
 #[derive(StructOpt)]
 enum Opt {
-    Info,
-    Search
+    Info { url: String },
+    Search { keyword: String }
+}
+
+#[derive(Deserialize)]
+struct InfoResponse {
+    data: StationInfo
+}
+
+#[derive(Deserialize)]
+struct StationInfo {
+    aqi: u32,
+    // attributions: serde_json::Value,
+    city: City,
+    // forecast: serde_json::Value,
+    // iaqi: serde_json::Value
+}
+
+#[derive(Deserialize)]
+struct City {
+    name: String,
+    // url: String
 }
 
 #[derive(Deserialize)]
@@ -33,7 +53,13 @@ struct SearchResponse {
 #[derive(Deserialize)]
 struct StationAqi {
     aqi: String,
-    station: serde_json::Value
+    station: Station
+}
+
+#[derive(Deserialize)]
+struct Station {
+    name: String,
+    url: String
 }
 
 #[tokio::main]
@@ -44,41 +70,38 @@ async fn main() -> Result<()> {
     let args = Aqi::from_args();
 
     match args.command {
-        Opt::Info => {
+        Opt::Info { url }=> {
             let response = client
-                .get("https://api.waqi.info/feed/beijing/")
+                .get(format!("https://api.waqi.info/feed/{}/", url))
                 .query(&[("token", args.api_token)])
                 .send()
                 .await?
-                .json::<SearchResponse>()
+                .json::<InfoResponse>()
                 .await?;
 
-                for station in response.data {
-                    print!("{}", station.aqi)
-                }
-            // println!("{}", serde_json::to_string(&response)?);
+            let StationInfo { city, aqi, .. } = response.data;
+            println!("{} aqi: {}", city.name, aqi);
         }
-        Opt::Search => {
+        Opt::Search { keyword } => {
             let response = client
                 .get("https://api.waqi.info/search/")
                 .query(&[
                     ("token", args.api_token),
-                    ("keyword", String::from("San Francisco"))
+                    ("keyword", keyword)
                 ])
                 .send()
                 .await?
                 .json::<SearchResponse>()
                 .await?;
 
-                for station in response.data {
-                    print!(
-                        "{} {}",
-                        station.aqi,
-                        station.station["name"]
-                            .as_str()
-                            .unwrap()
-                    )
-                }
+            for station in response.data {
+                print!(
+                    "{:2} {} {} \n",
+                    station.aqi,
+                    station.station.name,
+                    station.station.url,
+                )
+            }
         }
     }
 
